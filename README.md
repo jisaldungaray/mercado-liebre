4a24d28447180c0d55959d9652f764b3baebed1c
jisaldungaray <jisaldungaray@gmail.com>
2021-10-18 21:58:29 -0300

    Carga Mercado Liebre

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1>Mercado Liebre</h1>
-<h2>Proyecto E-Commerce by JIS</h2>
--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const express = require ('express');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
-const path = require ('path');
-const PUERTO = 4040;
 
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname,'./views/home.html'))
-})
+const publicPath = path.resolve('public');
 
-app.listen(PUERTO, () => {
-    console.log('Servidor Escuchando Spotify en 4040!!!')
-})
+app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get('/register.html', function(req, res) {
+    res.sendFile(path.resolve('./views/register.html'));
+});
+app.post('/register', function(req, res) {
+    res.send(req.body);
+});
+
+app.get('/login.html', function(req, res) {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+app.post('/login', function(req, res) {
+    res.send(req.body);
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor Corriendo en el puerto 3000')
+});
